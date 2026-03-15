ea6d15765624fe94cd311743781e356c0f74e1e3
XuanDe <69022261+Albertlearnpython@users.noreply.github.com>
2026-03-15 15:45:44 +0800

    Load Django ASGI app before websocket routes

--- blogsite/blogsite/asgi.py
+++ blogsite/blogsite/asgi.py
@@ -15,9 +15,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from blog.routing import websocket_urlpatterns
-
 django_asgi_application = get_asgi_application()
+
+from blog.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
